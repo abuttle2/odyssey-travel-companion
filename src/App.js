58a0aeecad5907a3469e0699c44ab186154d5dc2
Obee123 <aobiaderi@yahoo.com>
2023-03-17 17:55:43 +0000

    created navbar.css and added styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './Navbar.css';
 
 import Navbar from './components/pages/Navbar';
 // import Home from './Home';

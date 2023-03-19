50a38d4970fe7ef5b7bc28fe88e0de044e8ffe2f
Obee123 <aobiaderi@yahoo.com>
2023-03-19 14:19:07 +0000

    created function to render about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div id="about-container" className="abt jumbotron ">
+        <div className="col-sm-12 col-md-12 col-lg-12">
+            <h2 className="display-4 abt3">About</h2>
+            <p className="text-justify">Lorem ipsum.</p>
+        </div>
+        </div>
+    )
+}
+export default About;

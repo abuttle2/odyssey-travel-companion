560304f61395b2e3bdd21e7eccae92f1d876a1ed
Obee123 <aobiaderi@yahoo.com>
2023-03-19 15:09:54 +0000

    added image to about.js and created about.css file

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import travel5 from '../travel5.gif';
 
 function About() {
     return (
         <div id="about-container" className="abt jumbotron ">
-        <div className="col-sm-12 col-md-12 col-lg-12">
-            <h2 className="display-4 abt3">About</h2>
-            <p className="text-justify">Lorem ipsum.</p>
-        </div>
+            <div className="col-sm-12 col-md-9 col-lg-9">
+                <h2 className="display-4 abt3">About</h2>
+                <p className="text-justify">Odyssey Travel Companion provides tourists with up-to-date information on popular attractions, events, restaurants, and accommodations in the area, so that they can make informed decisions about how to spend their time and money during their trip. Users are able to search for and view detailed information on each item including descriptions and photos.</p>
+            </div>
+            <div className="col-sm-12 col-md-3 col-lg-3"> 
+            <img className="my-image4" src={travel5} alt="travel5"/>
+          </div>             
         </div>
     )
 }

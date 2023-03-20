1ef744599442b604edd2336fe4c2c6b4de296c70
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-03-20 11:09:01 +0000

    added footer file to component/pages

---
+++ src/components/pages/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Container, Row, Col } from 'react-bootstrap';
+
+
+
+function Footer () {
+    return (
+      <footer> 
+        <h1>Footer</h1> 
+      </footer>
+    );
+}
+
+export default Footer;

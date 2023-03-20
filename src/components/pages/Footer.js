725b5cee8ba8940be35750484bae0c32ff12f9e1
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-03-20 13:51:52 +0000

    added icons to the footer

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 
 
 function Footer () {
     return (
-      <footer className="footer"> 
+      <footer className="footer bg-light"> 
         <Container>
             <Row>
-                <Col>
-                    <p>&copy; Copyright © 2022 | All Rights Reserved.</p>
-                </Col>
+              <Col className='col-sm-12 col-md-6 col-lg-8'>
+                <p className='copyright-text'>&copy; Copyright © 2022 | All Rights Reserved.</p>          
+              </Col>
+                <Col className='col-sm-12 col-md-6 col-lg-4 text-center py-3'>
+                  <ul className='social_media-icons'>
+                    <li>
+                      <a href="https://github.com/Adriana-Camarotto">
+                        <FontAwesomeIcon icon={faGithub} /> Adriana
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://github.com/abuttle2">
+                        <FontAwesomeIcon icon={faGithub} /> Alex
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://github.com/willguille">
+                        <FontAwesomeIcon icon={faGithub} /> Guillermo
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://github.com/aobiaderi">
+                        <FontAwesomeIcon icon={faGithub} /> Obiaderi
+                      </a>   
+                    </li>
+                  </ul>                    
+              </Col>
             </Row>
-        </Container>
+          </Container>
   
       </footer>
     );

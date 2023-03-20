11ef5ee6be9858679560a0638193735045b79bf3
wilguille <willyguille97@gmail.com>
2023-03-20 16:04:23 +0000

    Created Map using google maps and deleted some useless files

---
+++ src/components/Map.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function Map() {
+  return (
+    <LoadScript
+      googleMapsApiKey="AIzaSyDm2wAUZtbatfRxowbpWSgRmMh_2Xq3iXY"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
+}
+
+export default React.memo(Map)

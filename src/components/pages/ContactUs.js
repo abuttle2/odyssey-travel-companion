dd5ea8bebae225c3afc1b462ea06af2253b01ac7
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-03-20 22:30:12 +0000

    Add form component with submit handler to handle form submission

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -11,7 +11,10 @@ function ContactUs() {
             <Container className="py-5">
                 <Row>
                     <Col className="col-sm-12 col-md-6 col-lg-4 mx-auto">
-                        <h3 className="text-form text-center mb-4">Get in Touch</h3>                      
+                        <h3 className="text-form text-center mb-4">Get in Touch</h3> 
+                        <Form onSubmit={handleSubmit}>
+                            
+                        </Form>                    
                     </Col>
                 </Row>
             </Container>            

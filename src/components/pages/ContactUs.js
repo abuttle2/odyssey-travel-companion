a0af219f30f5f093061559a99bf4283fb8bb6dd5
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-03-20 23:05:02 +0000

    Add form input field for email and message  with a label and onChange event handler to update the email and state variable. Defined useState and handleSubmit

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Container, Row, Col, } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col,Form } from 'react-bootstrap';
 
 
 function ContactUs() {
     const [name,setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e) =>{}
+    
   
         return (            
             <Container className="py-5">
@@ -13,7 +16,41 @@ function ContactUs() {
                     <Col className="col-sm-12 col-md-6 col-lg-4 mx-auto">
                         <h3 className="text-form text-center mb-4">Get in Touch</h3> 
                         <Form onSubmit={handleSubmit}>
-                            
+                            <Form.Group controlId="name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                required                             
+                            />
+                            </Form.Group>   
+
+                            <Form.Group controlId="email">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required                             
+                            />
+                            </Form.Group>
+
+                            <Form.Group controlId="message">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control
+                                type="textarea"
+                                rows={3}
+                                placeholder="Enter message"
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                                required                             
+                            />
+                            </Form.Group>
+
+
                         </Form>                    
                     </Col>
                 </Row>

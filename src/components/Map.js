92c490229b6b149e8cb36c405e52b26b7f32880f
wilguille <willyguille97@gmail.com>
2023-03-21 19:09:51 +0000

    Fixed map not rendering on page change

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,6 +8,26 @@ const containerStyle = {
 
 const libraries = ["places"];
 
+class LoadScriptOnlyIfNeeded extends LoadScript {
+  componentDidMount() {
+    const cleaningUp = true
+    const isBrowser = typeof document !== "undefined" // require('@react-google-maps/api/src/utils/isbrowser')
+    const isAlreadyLoaded = window.google && window.google.maps && document.querySelector('body.first-hit-completed') // AJAX page loading system is adding this class the first time the app is loaded
+    if (!isAlreadyLoaded && isBrowser) {
+      // @ts-ignore
+      if (window.google && !cleaningUp) {
+        console.error("google api is already presented")
+        return
+      }
+
+      this.isCleaningUp().then(this.injectScript)
+    }
+
+    if (isAlreadyLoaded) {
+      this.setState({ loaded: true })
+    }
+  }
+}
 
 function Map() {
   const [searchBox, setSearchBox] = useState(null);
@@ -33,7 +53,7 @@ function Map() {
   };
 
   return (
-    <LoadScript
+    <LoadScriptOnlyIfNeeded
       googleMapsApiKey="AIzaSyDm2wAUZtbatfRxowbpWSgRmMh_2Xq3iXY"
       libraries={libraries}
     >
@@ -87,7 +107,7 @@ function Map() {
           />
         ))}
       </GoogleMap>
-    </LoadScript>
+    </LoadScriptOnlyIfNeeded>
   );
 }
 

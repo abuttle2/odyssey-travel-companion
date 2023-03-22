d14e41c902ce0bf3dbb3e4e0ff43341baee89a75
Obee123 <aobiaderi@yahoo.com>
2023-03-22 13:46:00 +0000

    added media queries to home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,10 +13,10 @@ function Home() {
       <div className="image-container">
         <img className="my-image6" src={travel6} alt="travel6" />
         <div className="text-container">
-          <h1 className="display-4">
+          <h1 className="display-4 topic1">
             Odyssey: Your Ultimate Travel Guide 
           </h1>
-          <h2 className="lead">
+          <h2 className="lead topic2">
           For the latest updates on tourist attractions, events, restaurants and accommodation planning, choose our amazing resource.
           </h2>
         </div>
@@ -26,20 +26,7 @@ function Home() {
         <img className="my-image3" src={odyssey} alt="odyssey" />
         <img className="my-image5" src={travel5} alt="travel5" />
       </div> 
-      {/* <div className="row">
-                    <div className="col-sm-4 col-md-4 text-center">
 
-                        <img className="my-image" src={travel1} alt="travel1" />
-                    </div>
-                    <div className="col-sm-4 col-md-4 text-center">
-
-                        <img className="my-image3" src={odyssey} alt="odyssey" />
-                    </div>
-                    <div className="col-sm-4 col-md-4 text-center">
-
-                        <img className="my-image5" src={travel5} alt="travel5" />
-                    </div>
-                </div>  */}
       <div className="col-sm-12 col-md-12 col-lg-12 text-center download">
         <h2 className="section-heading abt4">
         Check out our About page to uncover a world of excitement! 

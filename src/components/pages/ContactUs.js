538b70fc1a29deeab8a49e516e467eaeb60b5339
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-03-22 19:15:24 +0000

    footer Style update

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -37,8 +37,8 @@ function ContactUs() {
     };
           
   
-        return (            
-            <Container id="form" className="form">
+        return (           
+            <Container id="form" className="form" style={{height: 600}} >
                 <Row className='form-row'>
                     <Col className="col-sm-4 col-md-4 col-lg-4 mx-auto">
                         <h3 className="text-form text-center mb-4">Get in Touch</h3>
@@ -92,7 +92,7 @@ function ContactUs() {
                         </Form>                    
                     </Col>
                 </Row>
-            </Container>            
+            </Container>    
     )
 }
 export default ContactUs;
